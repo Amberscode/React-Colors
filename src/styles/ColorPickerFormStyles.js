d6d10b3d55b5d64103111932e4fd66fff844f1e7
Amber <amber@ambers-air.lan>
2022-01-30 15:32:34 -0600

    first commit

---
+++ src/styles/ColorPickerFormStyles.js
@@ -0,0 +1,21 @@
+const styles = {
+  picker: {
+    width: "100% !important",
+    marginTop: "2rem",
+  },
+  container: {
+    width: "100%",
+  },
+  addColor: {
+    width: "100%",
+    padding: "0.5rem",
+    marginTop: "0.8rem",
+    fontSize: "1.5rem",
+  },
+  colorNameInput: {
+    width: "100%",
+    height: "60px",
+  },
+};
+
+export default styles;
